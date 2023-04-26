a30352cd129fe1d1cc916fa6c19b30d93d4754ab
muhammad9240 <muhammad92401111@gmail.com>
2023-04-26 14:10:40 +0500

    jdjdd

--- number_odd.dart
+++ number_odd.dart
@@ -4,4 +4,14 @@
 
 //Print the number of even digits in the variable "var_int".
 
-void main() {}
+void main() {
+    int a =9867;
+     int x1, x2, x3, x4;
+    x1 = a % 10;
+    x2 = a % 100 ~/ 10;
+    x3 = a ~/ 100 % 10;
+    x4 = a ~/ 1000;
+    int javob = 4 - x1 % 2 + x2 % 2 + x3 % 2 + x4 % 2;
+    print(javob);
+    
+}
