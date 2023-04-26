d0f849eac17c78acb33cad8de1a66d9501d86695
muhammad9240 <muhammad92401111@gmail.com>
2023-04-26 12:28:35 +0500

    sdhds

--- number_even.dart
+++ number_even.dart
@@ -4,4 +4,12 @@
 
 //Print the number of even digits in the variable "var_int".
 
-void main() {}
+void main() {
+    int a = 4325;
+    int x1, x2, x3, x4;
+    x1 = a % 10;
+    x2 = a % 100 ~/ 10;
+    x3 = a ~/ 100 % 10;
+    x4 = a ~/ 1000;
+    print(4 - x1 % 2 + x2 % 2 + x3 % 2 + x4 % 2);
+}
